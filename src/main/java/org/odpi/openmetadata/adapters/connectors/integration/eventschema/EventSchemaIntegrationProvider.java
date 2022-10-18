54f410de65bd7c0f0e06f55d23a86c91e1cb8458
Jürgen Hemelt <juergen.hemelt@atruvia.de>
2022-10-18 17:36:08 +0200

    Copyright comments
    Signed-off-by: Jürgen Hemelt <juergen.hemelt@atruvia.de>

--- src/main/java/org/odpi/openmetadata/adapters/connectors/integration/eventschema/EventSchemaIntegrationProvider.java
+++ src/main/java/org/odpi/openmetadata/adapters/connectors/integration/eventschema/EventSchemaIntegrationProvider.java
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: Apache-2.0 */
-/* Copyright Contributors to the ODPi Egeria project. */
+/* Copyright © 2021 Atruvia AG <opensource@atruvia.de> as contributor to the ODPi Egeria project. */
 
 package org.odpi.openmetadata.adapters.connectors.integration.eventschema;
 
